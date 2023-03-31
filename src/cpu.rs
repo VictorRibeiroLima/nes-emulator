10ea84771861f613915fd78ddd6b08ba716f5f03
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2023-03-31 00:18:17 -0300

    feat: memory

--- src/cpu.rs
+++ src/cpu.rs
@@ -39,6 +39,42 @@ pub struct CPU {
     memory: [u8; 0xFFFF],
 }
 
+trait Memory {
+    fn read_from_memory(&self, addr: u16) -> u8;
+
+    fn write_to_memory(&mut self, addr: u16, data: u8);
+
+    fn read_from_memory_le(&self, addr: u16) -> u16 {
+        let lo = self.read_from_memory(addr) as u16;
+        let hi = self.read_from_memory(addr + 1) as u16;
+        /*hi << 8 moves the value of the first half of this 16bit data to the second half
+          0b0000_0000_1111_1111 becomes 0b1111_1111_0000_0000
+        */
+        (hi << 8) | (lo as u16)
+    }
+
+    fn write_to_memory_le(&mut self, addr: u16, data: u16) {
+        /*data >> 8 moves the value of the second half of this 16bit data to the first half
+        0b1111_1111_0000_0000 becomes 0b0000_0000_1111_1111
+        */
+        let hi = (data >> 8) as u8;
+
+        //data & 0xff (0b0000_0000_1111_1111) just unset's the second half to make sure this conversion does't break
+        let lo = (data & 0xff) as u8;
+        self.write_to_memory(addr, lo);
+        self.write_to_memory(addr + 1, hi);
+    }
+}
+
+impl Memory for CPU {
+    fn read_from_memory(&self, addr: u16) -> u8 {
+        return self.memory[addr as usize];
+    }
+    fn write_to_memory(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+}
+
 impl CPU {
     pub fn new() -> Self {
         CPU {
@@ -127,35 +163,6 @@ impl CPU {
                 }
             }
         }
-    }
-
-    fn read_from_memory(&self, addr: u16) -> u8 {
-        return self.memory[addr as usize];
-    }
-
-    fn read_from_memory_le(&self, addr: u16) -> u16 {
-        let lo = self.read_from_memory(addr) as u16;
-        let hi = self.read_from_memory(addr + 1) as u16;
-        /*hi << 8 moves the value of the first half of this 16bit data to the second half
-          0b0000_0000_1111_1111 becomes 0b1111_1111_0000_0000
-        */
-        (hi << 8) | (lo as u16)
-    }
-
-    fn write_to_memory(&mut self, addr: u16, data: u8) {
-        self.memory[addr as usize] = data;
-    }
-
-    fn write_to_memory_le(&mut self, addr: u16, data: u16) {
-        /*data >> 8 moves the value of the second half of this 16bit data to the first half
-        0b1111_1111_0000_0000 becomes 0b0000_0000_1111_1111
-        */
-        let hi = (data >> 8) as u8;
-
-        //data & 0xff (0b0000_0000_1111_1111) just unset's the second half to make sure this conversion does't break
-        let lo = (data & 0xff) as u8;
-        self.write_to_memory(addr, lo);
-        self.write_to_memory(addr + 1, hi);
     }
 
     //This method will see the result of an operation and set the Z flag accordantly
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod cpu;
+
 fn main() {
     let mut cpu = cpu::CPU::new();
     cpu.load_and_run(vec![0xA9, 110, 0xAA, 0xE8, 0x00]);
