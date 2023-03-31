b95e198b71a48f469d73b9f99f6aca5b42e49b52
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2023-03-31 00:13:49 -0300

    feat: memory

--- src/cpu.rs
+++ src/cpu.rs
@@ -36,6 +36,7 @@ pub struct CPU {
     */
     pub status: u8,
     pub program_counter: u16,
+    memory: [u8; 0xFFFF],
 }
 
 impl CPU {
@@ -46,28 +47,53 @@ impl CPU {
             register_y: 0,
             status: 0,
             program_counter: 0,
+            memory: [0; 0xFFFF],
         }
     }
 
-    pub fn interpret(&mut self, program: Vec<u8>) {
+    pub fn load(&mut self, program: Vec<u8>) {
+        //loads the program into memory from 0x8000 addr until the len of the program
+        self.memory[0x8000..(0x8000 + program.len())].copy_from_slice(&program[..]);
+
+        //writes on the addr 0xfffc the addr of the beginning of the loaded program
+        self.write_to_memory_le(0xfffc, 0x8000);
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.register_y = 0;
+        self.status = 0;
+
+        //reads the addr of the beginning of the loaded program
+        self.program_counter = self.read_from_memory_le(0xFFFC);
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run();
+    }
+
+    pub fn run(&mut self) {
         loop {
-            let opcode_value = program[self.get_instruction()];
+            let opcode_value = self.next_instruction();
             let opcode = Opcodes::from_u8(opcode_value).expect("Valid opcode");
             match opcode {
                 Opcodes::LDA => {
-                    let param = program[self.get_instruction()];
+                    let param = self.next_instruction();
                     self.register_a = param;
                     self.update_negative_flag(param);
                     self.update_zero_flag(param);
                 }
                 Opcodes::LDX => {
-                    let param = program[self.get_instruction()];
+                    let param = self.next_instruction();
                     self.register_x = param;
                     self.update_negative_flag(param);
                     self.update_zero_flag(param);
                 }
                 Opcodes::LDY => {
-                    let param = program[self.get_instruction()];
+                    let param = self.next_instruction();
                     self.register_y = param;
                     self.update_negative_flag(param);
                     self.update_zero_flag(param);
@@ -103,6 +129,35 @@ impl CPU {
         }
     }
 
+    fn read_from_memory(&self, addr: u16) -> u8 {
+        return self.memory[addr as usize];
+    }
+
+    fn read_from_memory_le(&self, addr: u16) -> u16 {
+        let lo = self.read_from_memory(addr) as u16;
+        let hi = self.read_from_memory(addr + 1) as u16;
+        /*hi << 8 moves the value of the first half of this 16bit data to the second half
+          0b0000_0000_1111_1111 becomes 0b1111_1111_0000_0000
+        */
+        (hi << 8) | (lo as u16)
+    }
+
+    fn write_to_memory(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    fn write_to_memory_le(&mut self, addr: u16, data: u16) {
+        /*data >> 8 moves the value of the second half of this 16bit data to the first half
+        0b1111_1111_0000_0000 becomes 0b0000_0000_1111_1111
+        */
+        let hi = (data >> 8) as u8;
+
+        //data & 0xff (0b0000_0000_1111_1111) just unset's the second half to make sure this conversion does't break
+        let lo = (data & 0xff) as u8;
+        self.write_to_memory(addr, lo);
+        self.write_to_memory(addr + 1, hi);
+    }
+
     //This method will see the result of an operation and set the Z flag accordantly
     fn update_zero_flag(&mut self, result: u8) {
         if result == 0 {
@@ -125,8 +180,8 @@ impl CPU {
         }
     }
 
-    fn get_instruction(&mut self) -> usize {
-        let current_value = self.program_counter as usize;
+    fn next_instruction(&mut self) -> u8 {
+        let current_value = self.read_from_memory(self.program_counter);
         self.program_counter += 1;
         return current_value;
     }
@@ -139,7 +194,7 @@ mod test {
     #[test]
     fn test_0xa9_lda_immediately_load_data() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
         assert_eq!(cpu.register_a, 0x05);
         assert!(cpu.status & 0b0000_0010 == 0b0);
         assert!(cpu.status & 0b1000_0000 == 0);
@@ -148,14 +203,14 @@ mod test {
     #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
         assert!(cpu.status & 0b0000_0010 == 0b10);
     }
 
     #[test]
     fn test_0xa9_lda_negative_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0xff, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0xff, 0x00]);
         assert_eq!(cpu.register_a, 0xff);
         assert!(cpu.status & 0b0000_0010 == 0b00);
         assert!(cpu.status & 0b1000_0000 == 0x80);
@@ -164,7 +219,7 @@ mod test {
     #[test]
     fn test_0xa2_ldx_immediately_load_data() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa2, 0x05, 0x00]);
+        cpu.load_and_run(vec![0xa2, 0x05, 0x00]);
         assert_eq!(cpu.register_x, 0x05);
         assert!(cpu.status & 0b0000_0010 == 0b0);
         assert!(cpu.status & 0b1000_0000 == 0);
@@ -173,14 +228,14 @@ mod test {
     #[test]
     fn test_0xa2_ldx_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa2, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa2, 0x00, 0x00]);
         assert!(cpu.status & 0b0000_0010 == 0b10);
     }
 
     #[test]
     fn test_0xa2_ldx_negative_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa2, 0xff, 0x00]);
+        cpu.load_and_run(vec![0xa2, 0xff, 0x00]);
         assert_eq!(cpu.register_x, 0xff);
         assert!(cpu.status & 0b0000_0010 == 0b00);
         assert!(cpu.status & 0b1000_0000 == 0x80);
@@ -189,7 +244,7 @@ mod test {
     #[test]
     fn test_0xa0_ldy_immediately_load_data() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa0, 0x05, 0x00]);
+        cpu.load_and_run(vec![0xa0, 0x05, 0x00]);
         assert_eq!(cpu.register_y, 0x05);
         assert!(cpu.status & 0b0000_0010 == 0b0);
         assert!(cpu.status & 0b1000_0000 == 0);
@@ -198,14 +253,14 @@ mod test {
     #[test]
     fn test_0xa0_ldy_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa0, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa0, 0x00, 0x00]);
         assert!(cpu.status & 0b0000_0010 == 0b10);
     }
 
     #[test]
     fn test_0xa0_ldy_negative_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa0, 0xff, 0x00]);
+        cpu.load_and_run(vec![0xa0, 0xff, 0x00]);
         assert_eq!(cpu.register_y, 0xff);
         assert!(cpu.status & 0b0000_0010 == 0b00);
         assert!(cpu.status & 0b1000_0000 == 0x80);
@@ -215,7 +270,9 @@ mod test {
     fn test_0xaa_tax_copied_a_to_x() {
         let mut cpu = CPU::new();
         cpu.register_a = 10;
-        cpu.interpret(vec![0xaa, 0x00]);
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xaa, 0x00]);
+        cpu.run();
         assert_eq!(cpu.register_a, cpu.register_x);
         assert_eq!(cpu.register_x, 10);
     }
@@ -224,7 +281,9 @@ mod test {
     fn test_0xaa_tax_zero_flag() {
         let mut cpu = CPU::new();
         cpu.register_a = 0;
-        cpu.interpret(vec![0xaa, 0x00]);
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xaa, 0x00]);
+        cpu.run();
         assert!(cpu.status & 0b0000_0010 == 0b10);
     }
 
@@ -232,7 +291,9 @@ mod test {
     fn test_0xaa_tax_negative_flag() {
         let mut cpu = CPU::new();
         cpu.register_a = 0x80;
-        cpu.interpret(vec![0xaa, 0x00]);
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xaa, 0x00]);
+        cpu.run();
         assert!(cpu.status & 0b1000_0000 == 0x80);
     }
 
@@ -240,7 +301,9 @@ mod test {
     fn test_0xa8_tay_copied_a_to_y() {
         let mut cpu = CPU::new();
         cpu.register_a = 10;
-        cpu.interpret(vec![0xa8, 0x00]);
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xa8, 0x00]);
+        cpu.run();
         assert_eq!(cpu.register_a, cpu.register_y);
         assert_eq!(cpu.register_y, 10);
     }
@@ -249,7 +312,9 @@ mod test {
     fn test_0xa8_tay_zero_flag() {
         let mut cpu = CPU::new();
         cpu.register_a = 0;
-        cpu.interpret(vec![0xa8, 0x00]);
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xa8, 0x00]);
+        cpu.run();
         assert!(cpu.status & 0b0000_0010 == 0b10);
     }
 
@@ -257,7 +322,9 @@ mod test {
     fn test_0xa8_tay_negative_flag() {
         let mut cpu = CPU::new();
         cpu.register_a = 0x80;
-        cpu.interpret(vec![0xa8, 0x00]);
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xa8, 0x00]);
+        cpu.run();
         assert!(cpu.status & 0b1000_0000 == 0x80);
     }
 
@@ -265,7 +332,9 @@ mod test {
     fn test_0xe8_incremented_register_x() {
         let mut cpu = CPU::new();
         cpu.register_x = 0x5d;
-        cpu.interpret(vec![0xe8, 0x00]);
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xe8, 0x00]);
+        cpu.run();
         assert_eq!(cpu.register_x, 0x5e);
         assert!(cpu.status & 0b0000_0010 == 0b0);
     }
@@ -274,7 +343,9 @@ mod test {
     fn test_0xe8_incremented_negative_flag() {
         let mut cpu = CPU::new();
         cpu.register_x = 0xab;
-        cpu.interpret(vec![0xe8, 0x00]);
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xe8, 0x00]);
+        cpu.run();
         assert!(cpu.status & 0b1000_0000 == 0x80);
     }
 
@@ -282,8 +353,9 @@ mod test {
     fn test_0xe8_overflow() {
         let mut cpu = CPU::new();
         cpu.register_x = 0xff;
-        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
-
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xe8, 0xe8, 0x00]);
+        cpu.run();
         assert_eq!(cpu.register_x, 1)
     }
 
@@ -291,7 +363,9 @@ mod test {
     fn test_0xc8_incremented_register_y() {
         let mut cpu = CPU::new();
         cpu.register_y = 0x5d;
-        cpu.interpret(vec![0xc8, 0x00]);
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xc8, 0x00]);
+        cpu.run();
         assert_eq!(cpu.register_y, 0x5e);
         assert!(cpu.status & 0b0000_0010 == 0b0);
     }
@@ -300,7 +374,9 @@ mod test {
     fn test_0xc8_incremented_negative_flag() {
         let mut cpu = CPU::new();
         cpu.register_y = 0xab;
-        cpu.interpret(vec![0xc8, 0x00]);
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xc8, 0x00]);
+        cpu.run();
         assert!(cpu.status & 0b1000_0000 == 0x80);
     }
 
@@ -308,16 +384,32 @@ mod test {
     fn test_0xc8_overflow() {
         let mut cpu = CPU::new();
         cpu.register_y = 0xff;
-        cpu.interpret(vec![0xc8, 0xc8, 0x00]);
-
+        cpu.program_counter = 0x8000;
+        cpu.load(vec![0xc8, 0xc8, 0x00]);
+        cpu.run();
         assert_eq!(cpu.register_y, 1)
     }
 
     #[test]
     fn test_5_ops_working_together() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
 
         assert_eq!(cpu.register_x, 0xc1)
+    }
+
+    #[test]
+    fn test_reset_should_reset_cpu() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 10;
+        cpu.register_x = 11;
+        cpu.register_y = 12;
+        cpu.program_counter = 0xaab;
+        cpu.load(vec![0x00]);
+        cpu.reset();
+        assert_eq!(cpu.register_a, 0);
+        assert_eq!(cpu.register_x, 0);
+        assert_eq!(cpu.register_y, 0);
+        assert_eq!(cpu.program_counter, 0x8000);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod cpu;
 fn main() {
     let mut cpu = cpu::CPU::new();
-    cpu.interpret(vec![0xA9, 10, 0xAA, 0xE8, 0x00]);
+    cpu.load_and_run(vec![0xA9, 110, 0xAA, 0xE8, 0x00]);
     println!("{}", cpu.register_x);
+    println!("{}", cpu.register_a);
 }
